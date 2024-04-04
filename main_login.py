8192f8860a48af8f45e0923774d4c6b782fdce6e
mufaddal Hatim <mufaddalhatim53@gmail.com>
2024-04-04 13:01:37 +0500

    update on test code

--- main_login.py
+++ main_login.py
@@ -15,12 +15,12 @@ class main_login():
     
     def login(self):
         self.driver.maximize_window()
-        self.driver.get("https://qa.hazwoper-osha.com/login")
+        self.driver.get("https://hazwoper-osha.com/login")
         # Fill in the login form and submit it
         username_field = self.driver.find_element(By.ID, 'Login_username')
-        username_field.send_keys(self.username) # hazwoperosha4
+        username_field.send_keys(self.username)
         password_field = self.driver.find_element(By.ID, 'login_password')
-        password_field.send_keys(self.password) # yaAllah1#@
+        password_field.send_keys(self.password)
         password_field.send_keys(Keys.RETURN)
         WebDriverWait(driver=self.driver, timeout=10).until(
             lambda x: x.execute_script("return document.readyState === 'complete'")
@@ -31,110 +31,127 @@ class main_login():
 
     def click_course(self, driver):
         try:
-            driver.get("https://qa.hazwoper-osha.com/my-account/my-courses")
+            driver.get("https://hazwoper-osha.com/my-account/my-courses")
             time.sleep(5)
-            course_divs = driver.find_elements(By.CSS_SELECTOR, '.main_content .parentDiv')
+            lesson_divs = driver.find_elements(By.CLASS_NAME, 'parentDiv')
         except NoSuchElementException as e:
             print("No such course div element")
-        return (driver, course_divs)
+        return (driver, lesson_divs)
     
-    def click_lesson(self, driver):
+    def pass_final_quiz(self, driver, count):
         try:
-            driver.get("https://hazwoper-osha.com/lms/player/139")
-            time.sleep(5)
+            driver.find_element(By.ID, 'final-quiz').click()
+            time.sleep(3)
+            driver.find_elements(By.CLASS_NAME, 'answer-list-item')[0].click()
+            time.sleep(2)
+            driver.find_element(By.ID, 'finish_quiz').click()
+            question = driver.find_element(By.CLASS_NAME, 'question-heading').text
+            print(question)
+            options = driver.find_elements(By.CLASS_NAME, 'answer-list-item')
+            options_list = []
+            for option in options:
+                option_value = option.text
+                option_key = option.find_element(By.TAG_NAME, 'label').get_property("content")
+                print("options")
+                print(option_key, option_value)
+                key, value = option_value.split()[0], " ".join(option_value.split()[1:])
+                options_list.append((key, key+") "+value))
         except NoSuchElementException as e:
-            print("No such course div element")
-            
+            print("No such element")
+        time.sleep(2)
+        try:
+            driver.find_element(By.ID, 'continue_course').click()
+        except NoSuchElementException as e:
+            print("No such continue course element")
+        time.sleep(2)
         return driver
 
-    def get_div_title_val(self, driver):
-        if not self.flag:
-            self.login()
-        reg = r'[<|>|||:|"|/|\\|?|*]'
-        self.title_value = None
-        while True:
-            if self.title_value is not None:
-                print(self.title_value, self.class_values)
-                self.title_value_f = re.sub(reg, '-', title_value)
-                self.title_value_f = self.title_value_f.strip()
-                break
-            driver.get("https://icert.puresafety.com/Home/Dashboard")
-            WebDriverWait(driver=driver, timeout=20).until(
-                lambda x: x.execute_script("return document.readyState === 'complete'")
-            )
-            while True:
+    def click_lesson(self, driver, lesson_divs):
+        try:
+            lesson_links = [lesson_div.find_element(By.TAG_NAME, 'a').get_attribute("href") for lesson_div in lesson_divs]
+            print(lesson_links)
+            for lesson_link in lesson_links:
+                driver.get(lesson_link)
+                time.sleep(5)
+                print("Lesson Div clicked")
                 try:
-                    loading_div = driver.find_element(By.CSS_SELECTOR, 'div.x-mask-loading')
-                    time.sleep(3)
-                except NoSuchElementException:
-                    break
-            time.sleep(5)
-            page_source = driver.page_source
-            soup = BeautifulSoup(page_source, "html.parser")
-            # Get a tag in td with class "xgrid3-cell-first"
-            # Get the href attribute of the a tag
-            td_list = soup.find_all("td", class_="x-grid3-cell-first")
-            a_list = []
-            for td in td_list:
-                a_list.append(td.find('a'))
-            # filter None from a_list
-            a_list = list(filter(None, a_list))
-            elem = a_list[1]
-            title_value = elem.get('title')
-            class_values = elem.get('class')
-            # for title_value in title_values:
-            self.title_value = title_value
-            self.class_values = class_values
-        return driver, self.title_value, self.title_value_f, self.class_values
+                    driver.find_element(By.CLASS_NAME, 'learndash-resume-button').click()
+                    time.sleep(5)
+                    print("Lesson Resumed")
+                    while True:
+                        try:
+                            class_values = driver.find_element(By.CLASS_NAME, 'player-div')
+                        except NoSuchElementException as e:
+                            print("No such element player-div element found")
+                            class_values = None
+                        class_values = class_values.get_attribute("class") if class_values else []
+                        if "QuizMainDiv" in class_values:
+                            print("QuizMainDiv")
+                            driver.find_element(By.ID, 'lesson-quiz').click()
+                            time.sleep(5)
+                            count = driver.find_element(By.CLASS_NAME, 'question-count').text
+                            print(count)
+                            count = int(count.split()[-1].strip())
+                            print(count)
+                            driver = self.parse_quiz(driver, count)
+                        time.sleep(2)
+                        try:
+                            driver.find_element(By.ID, 'play_topic_slide').click()
+                        except NoSuchElementException as e:
+                            print("No such element play topic slide element found")
+
+                        if "QuizMainDiv2" in class_values:
+                            print("in QuizMainDiv2")
+                            try:
+                                driver.find_element(By.ID, 'final-quiz').click()
+                                time.sleep(3)
+                                count = driver.find_element(By.CLASS_NAME, 'question-count').text
+                                print(count)
+                                count = int(count.split()[-1].strip())
+                                print(count)
+                                driver = self.pass_final_quiz(driver, count)
+                                break
+                            except NoSuchElementException as e:
+                                print("No such element next topic slide element found")
+                        time.sleep(2)
+                except NoSuchElementException as e:
+                    print("No such element play topic slide element found")
+        except NoSuchElementException as e:
+            print("No such course div element")
+        return driver
+
+    def parse_quiz(self, driver, count):
+        for i in range(count-1):
+            try:
+                driver.find_elements(By.CLASS_NAME, 'answer-list-item')[0].click()
+                time.sleep(2)
+                driver.find_element(By.ID, 'next_quiz').click()
+            except NoSuchElementException as e:
+                print("No such element")
+            time.sleep(3)
+        try:
+            driver.find_elements(By.CLASS_NAME, 'answer-list-item')[0].click()
+            time.sleep(2)
+            driver.find_element(By.ID, 'finish_quiz').click()
+        except NoSuchElementException as e:
+            print("No such element")
+        time.sleep(2)
+        try:
+            driver.find_element(By.ID, 'continue_course').click()
+        except NoSuchElementException as e:
+            print("No such continue course element")
+        time.sleep(2)
+        return driver
 
 if __name__ == "__main__":
-    # scrapers = [i.split('.')[0] for i in os.listdir() if i.endswith('ucert.py')]
     parser = argparse.ArgumentParser()
     parser.add_argument('--u', help='Username for login', required=True)
     parser.add_argument('--p', help='Password for login', required=True)
     args = parser.parse_args()
+    print(args)
     obj = main_login(args.u, args.p)
     driver = obj.login()
     time.sleep(5)
-    # driver, course_divs = obj.click_course(driver)
-    # course_divs[0].find_element(By.CSS_SELECTOR, 'button').click()
-    # time.sleep(5)
-    # course_resume = driver.find_elements(By.CSS_SELECTOR, '.learndash-resume-button')
-    # course_resume[0].click()
-    # time.sleep(5)
-    driver = obj.click_lesson(driver)
+    driver, lesson_divs = obj.click_course(driver)
+    driver = obj.click_lesson(driver, lesson_divs)
     time.sleep(5)
-    while True:
-        class_values = driver.find_element(By.CSS_SELECTOR, 'div#player-div').get('class')
-        if "QuizMainDiv" in class_values:
-            print("QuizMainDiv")
-            driver.find_element(By.CSS_SELECTOR, '#lesson-quiz').click()
-            time.sleep(5)
-            break
-        driver.find_element(By.CSS_SELECTOR, '#play_topic_slide').click()
-
-    time.sleep(5)
-
-    """
-    while True:
-        driver, title_value, title_value_f, class_values = obj.get_div_title_val(driver)
-        print("Driver, title_value, title_value_f, class_values")
-        print(driver, title_value, title_value_f, class_values)
-        time.sleep(5)
-        if "training-ThirdParty" in class_values:
-            scraper_mod = importlib.import_module("lesson_ucert")
-            scraper_class = getattr(scraper_mod, "lesson_ucert")
-            scraper_obj = scraper_class(driver, title_value, title_value_f)
-            scraper_obj.switch_to_iframe()
-            driver = scraper_obj.main()
-        if "training-Test" in class_values:
-            scraper_mod = importlib.import_module("test_ucert")
-            scraper_class = getattr(scraper_mod, "test_ucert")
-            scraper_obj = scraper_class(driver, title_value)
-            driver = scraper_obj.quiz_start()
-        if "training-Url" in class_values:
-            scraper_mod = importlib.import_module("doc_ucert")
-            scraper_class = getattr(scraper_mod, "doc_ucert")
-            scraper_obj = scraper_class(driver, title_value)
-            driver = scraper_obj.click_done()
-    """
--- test.py
+++ test.py
@@ -1,11 +1,21 @@
 import os
-from bs4 import BeautifulSoup
-def sorted_ls(path):
-    mtime = lambda f: os.stat(os.path.join(path, f)).st_mtime
-    return list(sorted(os.listdir(path), key=mtime))
+from all_imports import *
 
-html_files = os.path.join("scorm_packages", "Active Shooter- Law Enforcement", "page_100.html")
-soup = BeautifulSoup(open(html_files), 'html.parser', from_encoding='utf-8')
-transcription = soup.find('p', {'id': 'transcript_body'}).get_text()
-curr_data = " ".join([i.get_text() for i in soup.find_all('div', class_="dki-element-text")])
-print(curr_data)
+driver = webdriver.Edge(executable_path="msedgedriver.exe")
+
+html = """
+<div _ngcontent-wtp-c277="" class="answers-list align_right ng-tns-c277-54"><label _ngcontent-wtp-c277="" for="" class="question-count ng-tns-c277-54">QUESTION 1 OF 28</label><h3 _ngcontent-wtp-c277="" class="question-heading ng-tns-c277-54"><span _ngcontent-wtp-c277="" class="question-heading main-question-body ng-tns-c277-54">The words <b>“FUEL OIL”</b> may be used in place of the word “____________” on a placard that is displayed on a cargo tank or portable tank being used to transport fuel oil (not classed as a flammable liquid) by highway.</span><div _ngcontent-wtp-c277="" class="question-image ng-tns-c277-54 ng-trigger ng-trigger-slide"><!----><img _ngcontent-wtp-c277="" class="zoomable-image ng-tns-c277-54" src="" alt="undefined" style="width: 100%;"></div></h3><form _ngcontent-wtp-c277="" novalidate="" style="padding-bottom: 1px; padding-top: 10px;" class="ng-tns-c277-54 ng-untouched ng-pristine ng-invalid"><ul _ngcontent-wtp-c277="" class="wpProQuiz_questionList ng-tns-c277-54"><li _ngcontent-wtp-c277="" class="wpProQuiz_questionListItem answer-list-item ng-tns-c277-54 ng-star-inserted"><label _ngcontent-wtp-c277="" class="ng-tns-c277-54"><input _ngcontent-wtp-c277="" formcontrolname="option" type="radio" class="wpProQuiz_questionInput ng-tns-c277-54 ng-untouched ng-pristine ng-invalid" id="5607"><span _ngcontent-wtp-c277="" class="checkmark ng-tns-c277-54"></span> COMBUSTIBLE  </label></li><li _ngcontent-wtp-c277="" class="wpProQuiz_questionListItem answer-list-item ng-tns-c277-54 ng-star-inserted"><label _ngcontent-wtp-c277="" class="ng-tns-c277-54"><input _ngcontent-wtp-c277="" formcontrolname="option" type="radio" class="wpProQuiz_questionInput ng-tns-c277-54 ng-untouched ng-pristine ng-invalid" id="5605"><span _ngcontent-wtp-c277="" class="checkmark ng-tns-c277-54"></span> FLAMMABLE </label></li><li _ngcontent-wtp-c277="" class="wpProQuiz_questionListItem answer-list-item ng-tns-c277-54 ng-star-inserted"><label _ngcontent-wtp-c277="" class="ng-tns-c277-54"><input _ngcontent-wtp-c277="" formcontrolname="option" type="radio" class="wpProQuiz_questionInput ng-tns-c277-54 ng-untouched ng-pristine ng-invalid" id="5606"><span _ngcontent-wtp-c277="" class="checkmark ng-tns-c277-54"></span> NON-FLAMMABLE </label></li><!----></ul></form><div _ngcontent-wtp-c277="" class="footer-btn align_right ng-tns-c277-54"><button _ngcontent-wtp-c277="" type="button" id="next_quiz" aria-label="Next" class="ng-tns-c277-54 ng-star-inserted">Next</button><!----><!----></div></div>
+"""
+
+driver.get("data:text/html;charset=utf-8,{html_content}".format(html_content=html))
+time.sleep(5)
+question = driver.find_element(By.CLASS_NAME, 'question-heading').text
+print(question)
+options = driver.find_elements(By.CLASS_NAME, 'answer-list-item')
+options_list = []
+for option in options:
+    option_value = option.text
+    option_key = option.find_element(By.TAG_NAME, 'label').get_property("content")
+    print(option_key, option_value)
+    key, value = option_value.split()[0], " ".join(option_value.split()[1:])
+    options_list.append((key, key+") "+value))
